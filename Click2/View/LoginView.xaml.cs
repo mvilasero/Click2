192416e885357cf0073857b6ee7ae4cd86963794
Alvaro Costa <alvaro130303@gmail.com>
2023-12-04 18:10:31 +0100

    Sin errores pero no ejecuta

--- Click2/View/LoginView.xaml.cs
+++ Click2/View/LoginView.xaml.cs
@@ -31,5 +31,20 @@ namespace Click2.View
                 DragMove();
             }
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
