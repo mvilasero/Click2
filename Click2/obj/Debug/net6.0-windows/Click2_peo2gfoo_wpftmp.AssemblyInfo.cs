079c1c4a09875e56afda699a858823d6c6918a7b
Mateo Vilasero <m.vilaseromoya@gmail.com>
2023-12-04 18:24:14 +0100

    Ahora ejecuta y se ve xd

--- Click2/obj/Debug/net6.0-windows/Click2_peo2gfoo_wpftmp.AssemblyInfo.cs
+++ Click2/obj/Debug/net6.0-windows/Click2_peo2gfoo_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Click2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1bb1fd905bb213572a4c34cedb275d920d54e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a14f8da24cc308bcb22904b5c998f69245d0b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Click2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Click2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
